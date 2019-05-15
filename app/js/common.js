228c2016bcf355599331fb27d4c9586d0be34978
AndrewLymar <andriilymar@gmail.com>
2019-05-15 12:01:33 +0300

    Update

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,23 @@
 $(function () {
-
+	$('.reviews-slider').slick({
+		autoplay: true,
+		arrows: false,
+		slidesToShow: 2,
+		dots: false,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+				}
+    		}
+  		]
+	});
+	$('.menu').mobileMenu({
+		menuIconClassName: ".menu__icon",
+		mobileResolution: 768,
+		menuType: "sticky",
+		offsetToSticky: 50,
+		closeIconClassName: ""
+	});
 });
